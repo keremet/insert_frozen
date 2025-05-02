402e205dbbb5865458199902364af91e25dc8841
Andrey Sokolov <keremet@solaris.kirov.ru>
2025-05-02 18:54:37 +0300

    Add a check for an invalid type

--- insert_frozen.c
+++ insert_frozen.c
@@ -10,16 +10,22 @@ PG_FUNCTION_INFO_V1(insert_frozen);
 Datum
 insert_frozen(PG_FUNCTION_ARGS)
 {
-	Oid				row_type = get_fn_expr_argtype(fcinfo->flinfo, 0);
+	Oid row_type = get_fn_expr_argtype(fcinfo->flinfo, 0);
+	if (unlikely(InvalidOid == row_type))
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+				errmsg("Error getting the argument type"),
+				errhint("Is the argument passed?")));
+
 	TypeCacheEntry *tce = lookup_type_cache(row_type, 0);
-	if (tce->typtype != TYPTYPE_COMPOSITE)
+	if (unlikely(tce->typtype != TYPTYPE_COMPOSITE))
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("The argument type must be composite"),
 				errhint("Example: call %s((1, 't')::tableName);", __func__)));
 
-	Relation		rel = relation_open(tce->typrelid, RowExclusiveLock);
-	if (rel->rd_rel->relkind != 'r')
+	Relation rel = relation_open(tce->typrelid, RowExclusiveLock);
+	if (unlikely(rel->rd_rel->relkind != 'r'))
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("The argument type must refer to an ordinary table")));

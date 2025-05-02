24bfd9de88189e7b92030293b78b802dfc596138
Andrey Sokolov <keremet@solaris.kirov.ru>
2025-05-02 23:50:18 +0300

    Add primary key support

--- insert_frozen.c
+++ insert_frozen.c
@@ -1,6 +1,8 @@
 #include "postgres.h"
 #include "access/heapam.h"
 #include "catalog/pg_type.h"
+#include "executor/executor.h"
+#include "nodes/execnodes.h"
 #include "utils/typcache.h"
 
 PG_MODULE_MAGIC;
@@ -24,7 +26,12 @@ insert_frozen(PG_FUNCTION_ARGS)
 				errmsg("The argument type must be composite"),
 				errhint("Example: call %s((1, 't')::tableName);", __func__)));
 
-	Relation rel = relation_open(tce->typrelid, RowExclusiveLock);
+	/* 
+	 * Protect the table against concurrent data changes to avoid a new tuple
+	 * with the same unique key insertion in the period between index constraints
+	 * have already been checked and a new tuple has not yet been inserted.
+	 */
+	Relation rel = relation_open(tce->typrelid, ShareLock);
 	if (unlikely(rel->rd_rel->relkind != 'r'))
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
@@ -32,10 +39,43 @@ insert_frozen(PG_FUNCTION_ARGS)
 
 	TupleTableSlot *slot = MakeTupleTableSlot(rel->rd_att, &TTSOpsVirtual);
 	ExecStoreHeapTupleDatum(PG_GETARG_DATUM(0), slot);
-	table_tuple_insert(rel, slot, GetCurrentCommandId(true), TABLE_INSERT_FROZEN, NULL);
-	ExecDropSingleTupleTableSlot(slot);
 
-	relation_close(rel, RowExclusiveLock);
+	EState		 *estate = CreateExecutorState();
+	ResultRelInfo resultRelInfo;
+	InitResultRelInfo(&resultRelInfo, rel, 0, NULL, 0);
+	ExecOpenIndices(&resultRelInfo, true /* for ExecCheckIndexConstraints */);
+	if (rel->rd_att->constr)
+		ExecConstraints(&resultRelInfo, slot, estate);
+
+	/*
+	 * An error on unique constraint violation in ExecInsertIndexTuples cannot
+	 * rollback the insertion into a table, because of the TABLE_INSERT_FROZEN
+	 * option. So it is necessary to check index constraints before the insertion.
+	 */
+	ItemPointerData conflictTid;
+	if (!ExecCheckIndexConstraints(&resultRelInfo, slot, estate, &conflictTid, NIL))
+		ereport(ERROR,
+				(errcode(ERRCODE_UNIQUE_VIOLATION),
+				 errmsg("duplicate key value violates unique constraint \"%s\"",
+						RelationGetRelationName(rel)),
+				 errtableconstraint(rel, RelationGetRelationName(rel))));
+
+	table_tuple_insert(rel, slot, GetCurrentCommandId(true), TABLE_INSERT_FROZEN, NULL);
+
+	/* 
+	 * We need slot->tts_tid to update indexes. The slot->tts_tid value is set in
+	 * table_tuple_insert, so it should be called before ExecInsertIndexTuples.
+	 */
+	if (rel->rd_rel->relhasindex)
+	{
+		list_free(ExecInsertIndexTuples(&resultRelInfo, slot, estate, false,
+										false, NULL, NIL, false));
+	}
+
+	ExecCloseIndices(&resultRelInfo);
+	ExecDropSingleTupleTableSlot(slot);
+	relation_close(rel, ShareLock);
+	FreeExecutorState(estate);
 
 	PG_RETURN_VOID();
 }

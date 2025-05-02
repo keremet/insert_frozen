1e4dc91389af805712e0e5b17bd661f9c13425d8
Andrey Sokolov <keremet@solaris.kirov.ru>
2025-05-02 15:43:40 +0300

    Fix segfault when int is sent as the argument

--- insert_frozen.c
+++ insert_frozen.c
@@ -10,12 +10,7 @@ PG_FUNCTION_INFO_V1(insert_frozen);
 Datum
 insert_frozen(PG_FUNCTION_ARGS)
 {
-	Relation		rel;
-	HeapTupleHeader	row = DatumGetHeapTupleHeader(PG_GETARG_DATUM(0));
 	Oid				row_type = get_fn_expr_argtype(fcinfo->flinfo, 0);
-	HeapTupleData 	tup = { .t_len	= HeapTupleHeaderGetDatumLength(row),
-							.t_data	= row };
-
 	TypeCacheEntry *tce = lookup_type_cache(row_type, 0);
 	if (tce->typtype != TYPTYPE_COMPOSITE)
 		ereport(ERROR,
@@ -23,12 +18,15 @@ insert_frozen(PG_FUNCTION_ARGS)
 				errmsg("The argument type must be composite"),
 				errhint("Example: select %s((1, 't')::tableName);", __func__)));
 
-	rel = relation_open(tce->typrelid, RowExclusiveLock);
+	Relation		rel = relation_open(tce->typrelid, RowExclusiveLock);
 	if (rel->rd_rel->relkind != 'r')
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 				errmsg("The argument type must refer to an ordinary table")));
 
+	HeapTupleHeader	row = DatumGetHeapTupleHeader(PG_GETARG_DATUM(0));
+	HeapTupleData 	tup = { .t_len	= HeapTupleHeaderGetDatumLength(row),
+							.t_data	= row };
 	heap_insert(rel, &tup, GetCurrentCommandId(true), HEAP_INSERT_FROZEN, NULL);
 
 	relation_close(rel, RowExclusiveLock);
